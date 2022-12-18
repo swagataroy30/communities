241a9efc3f3b53c4135b9101a22bdd83ff869b37
Swagata Roy <swagataroy534@gmail.com>
2022-12-18 14:22:37 +0530

    Revert "feat: added firebase and chatscreen"
    This reverts commit f077163bca1f860a9448b351f02210b05d41a50c.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,81 +1,40 @@
-// import React, { useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 // import { signInWithEmailAndPassword } from 'firebase/auth';
-// import InputControl from '../Components/Home/InputControl';
+
+import InputControl from '../Components/Home/InputControl';
 // import { auth } from '../../firebase';
 
-// import styles from './Login.module.css';
-
-// function Login() {
-//   const navigate = useNavigate();
-//   const [values, setValues] = useState({
-//     email: '',
-//     pass: '',
-//   });
-//   const [errorMsg, setErrorMsg] = useState('');
-//   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
-
-//   const handleSubmission = () => {
-//     if (!values.email || !values.pass) {
-//       setErrorMsg('Fill all fields');
-//       return;
-//     }
-//     setErrorMsg('');
-
-//     setSubmitButtonDisabled(true);
-//     signInWithEmailAndPassword(auth, values.email, values.pass)
-//       .then(async (res) => {
-//         setSubmitButtonDisabled(false);
-
-//         navigate('/');
-//       })
-//       .catch((err) => {
-//         setSubmitButtonDisabled(false);
-//         setErrorMsg(err.message);
-//       });
-//   };
-// }
-
-// export default Login;
-
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
-//import InputControl from "../InputControl/InputControl";
-import InputControl from '../Components/Home/InputControl';
-import { auth } from "../firebase";
-
-import styles from "./Login.module.css";
+import styles from './Login.module.css';
 
 function Login() {
   const navigate = useNavigate();
   const [values, setValues] = useState({
-    email: "",
-    pass: "",
+    email: '',
+    pass: '',
   });
-  const [errorMsg, setErrorMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState('');
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
 
   const handleSubmission = () => {
     if (!values.email || !values.pass) {
-      setErrorMsg("Fill all fields");
+      setErrorMsg('Fill all fields');
       return;
     }
-    setErrorMsg("");
+    setErrorMsg('');
 
     setSubmitButtonDisabled(true);
-    signInWithEmailAndPassword(auth, values.email, values.pass)
-      .then(async (res) => {
-        setSubmitButtonDisabled(false);
-        
-        navigate("/chatscreen");
-      })
-      .catch((err) => {
-        setSubmitButtonDisabled(false);
-        setErrorMsg(err.message);
-      });
+    // signInWithEmailAndPassword(auth, values.email, values.pass)
+    //   .then(async (res) => {
+    //     setSubmitButtonDisabled(false);
+
+    //     navigate('/');
+    //   })
+    //   .catch((err) => {
+    //     setSubmitButtonDisabled(false);
+    //     setErrorMsg(err.message);
+    //   });
   };
-  
   return (
     <div className={styles.container}>
       <img src={require('../assets/login-img.jpg')} alt='signup-img' />
@@ -99,7 +58,7 @@ function Login() {
             Login
           </button>
           <p>
-            Don't have an account?{' '}
+            Already have an account?{' '}
             <span>
               <Link to='/signup'>Sign up</Link>
             </span>
@@ -111,4 +70,3 @@ function Login() {
 }
 
 export default Login;
-
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,84 +1,39 @@
-// import React, { useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import InputControl from '../Components/Home/InputControl';
-// import styles from './Signup.module.css';
-
-// function Signup() {
-//   const navigate = useNavigate();
-//   const [values, setValues] = useState({
-//     name: '',
-//     email: '',
-//     pass: '',
-//   });
-//   const [errorMsg, setErrorMsg] = useState('');
-//   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
-
-//   const handleSubmission = () => {
-//     if (!values.name || !values.email || !values.pass) {
-//       setErrorMsg('Fill all fields');
-//       return;
-//     }
-//     setErrorMsg('');
-
-//     setSubmitButtonDisabled(true);
-//     createUserWithEmailAndPassword(auth, values.email, values.pass)
-//       .then(async (res) => {
-//         setSubmitButtonDisabled(false);
-//         const user = res.user;
-//         await updateProfile(user, {
-//           displayName: values.name,
-//         });
-//         navigate('/');
-//       })
-//       .catch((err) => {
-//         setSubmitButtonDisabled(false);
-//         setErrorMsg(err.message);
-//       });
-//   };
-// }
-
-// export default Signup;
-
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import InputControl from '../Components/Home/InputControl';
-//import InputControl from "../InputControl/InputControl";
-import { auth } from "../firebase";
-
-import styles from "./Signup.module.css";
+import styles from './Signup.module.css';
 
 function Signup() {
   const navigate = useNavigate();
   const [values, setValues] = useState({
-    name: "",
-    email: "",
-    pass: "",
+    name: '',
+    email: '',
+    pass: '',
   });
-  const [errorMsg, setErrorMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState('');
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
 
   const handleSubmission = () => {
     if (!values.name || !values.email || !values.pass) {
-      setErrorMsg("Fill all fields");
+      setErrorMsg('Fill all fields');
       return;
     }
-    setErrorMsg("");
+    setErrorMsg('');
 
     setSubmitButtonDisabled(true);
-    createUserWithEmailAndPassword(auth, values.email, values.pass)
-      .then(async (res) => {
-        setSubmitButtonDisabled(false);
-        const user = res.user;
-        await updateProfile(user, {
-          displayName: values.name,
-        });
-        navigate("/");
-      })
-      .catch((err) => {
-        setSubmitButtonDisabled(false);
-        setErrorMsg(err.message);
-      });
+    // createUserWithEmailAndPassword(auth, values.email, values.pass)
+    //   .then(async (res) => {
+    //     setSubmitButtonDisabled(false);
+    //     const user = res.user;
+    //     await updateProfile(user, {
+    //       displayName: values.name,
+    //     });
+    //     navigate('/');
+    //   })
+    //   .catch((err) => {
+    //     setSubmitButtonDisabled(false);
+    //     setErrorMsg(err.message);
+    //   });
   };
 
   return (
@@ -121,4 +76,3 @@ function Signup() {
 }
 
 export default Signup;
-
